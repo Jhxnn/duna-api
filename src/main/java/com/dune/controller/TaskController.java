4f4e3fe6492cce1178c5ec915665af605116d96d
Jhxnn <jhonalizan@gmail.com>
2026-01-17 10:27:07 -0300

    feat: creating TaskController

--- src/main/java/com/dune/controller/TaskController.java
+++ src/main/java/com/dune/controller/TaskController.java
@@ -1,8 +1,14 @@
 package com.dune.controller;
 
+import com.dune.dto.TaskDto;
 import com.dune.service.TaskService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RequestMapping("/tasks")
 @RestController
@@ -10,6 +16,12 @@ public class TaskController {
     private final TaskService taskService;
     public TaskController(TaskService taskService){
         this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> createTask(@RequestBody TaskDto taskDto) {
+        taskService.requestCreate(taskDto);
+        return ResponseEntity.ok(Map.of("status", "Task creation requested successfully"));
     }
 
 }
