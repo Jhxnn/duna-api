66738c028709f211b7b3c16abf7848d9fe2937e3
Jhxnn <jhonalizan@gmail.com>
2026-01-18 10:24:33 -0300

    feat: TaskUpdate Controller

--- src/main/java/com/dune/controller/TaskController.java
+++ src/main/java/com/dune/controller/TaskController.java
@@ -1,12 +1,10 @@
 package com.dune.controller;
 
 import com.dune.dto.TaskDto;
+import com.dune.dto.TaskUpdateDto;
 import com.dune.service.TaskService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -22,6 +20,12 @@ public class TaskController {
     public ResponseEntity<Map<String, String>> createTask(@RequestBody TaskDto taskDto) {
         taskService.requestCreate(taskDto);
         return ResponseEntity.ok(Map.of("status", "Task creation requested successfully"));
+    }
+
+    @PutMapping
+    public ResponseEntity<Map<String, String>> updateTask(@RequestBody TaskUpdateDto taskDto) {
+        taskService.requestUpdate(taskDto);
+        return ResponseEntity.ok(Map.of("status", "Task update requested successfully"));
     }
 
 }
