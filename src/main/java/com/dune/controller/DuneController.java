763a5cef6fd1605434f39a000def096ac9d72a63
Jhxnn <jhonalizan@gmail.com>
2026-01-24 11:11:54 -0300

    feat: duneController

--- src/main/java/com/dune/controller/DuneController.java
+++ src/main/java/com/dune/controller/DuneController.java
@@ -1,4 +1,25 @@
 package com.dune.controller;
 
+import com.dune.dto.DuneResponseDto;
+import com.dune.service.DuneService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dunes")
 public class DuneController {
+
+    private final DuneService duneService;
+
+    public DuneController(DuneService duneService) {
+        this.duneService = duneService;
+    }
+
+    @GetMapping
+    public ResponseEntity<DuneResponseDto> getDuneSummary() {
+        DuneResponseDto duneSummary = duneService.getDuneSummary();
+        return ResponseEntity.ok(duneSummary);
+    }
 }
